ae07b46f6a14a9a4660a47e65ad8ebc2232c7f8f
adrichdmello <adrichdmello@gmail.com>
2012-10-25 09:38:28 +0400

    Thread

--- elevator1024/src/Control.java
+++ elevator1024/src/Control.java
@@ -10,19 +10,19 @@ public class Control {
 		building bldg = new building(10);
 		
 		//bldg.floors.get(5).fpanel.ecallBtn.press();
-	
+		//bldg.elevator.elevpanel.ePanel.get(3).fBtnPress();
+		//bldg.floors.get(7).fpanel.ecallBtn.press();
+		//bldg.elevator.elevpanel.ePanel.get(9).fBtnPress();
+		
 		ElevCallButton ev5 =	new ElevCallButton(5);
 		new Thread(ev5).start();
-		
-		//bldg.elevator.elevpanel.ePanel.get(3).fBtnPress();
+				
 		ElevCallButton ev3 =	new ElevCallButton(3);
 		new Thread(ev3).start();
 		
-		//bldg.floors.get(7).fpanel.ecallBtn.press();
 		ElevCallButton ev7 =	new ElevCallButton(7);
 		new Thread(ev7).start();
 		
-		//bldg.elevator.elevpanel.ePanel.get(9).fBtnPress();
 		ElevCallButton ev9 =	new ElevCallButton(9);
 		new Thread(ev9).start();
 		
