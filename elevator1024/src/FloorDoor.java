0a102c41d4570d713791e92af13c3755a989d47e
adrichdmello <adrichdmello@gmail.com>
2012-10-25 09:54:20 +0400

    method extracted in FloorDoor

--- elevator1024/src/FloorDoor.java
+++ elevator1024/src/FloorDoor.java
@@ -7,16 +7,30 @@ public class FloorDoor implements Door{
 	{
 		if(this.fDoorState)
 		{
-			this.fDoorState = false;
-			System.out.println("Floor door closed");
+			floorDoorClose();
 		}
 		else
 		{
-			this.fDoorState = true;
-			System.out.println("Floor door opened");
+			floorDoorOpen();
 		}
 		
 		return fDoorState;
+	}
+
+	/**
+	 * 
+	 */
+	public void floorDoorOpen() {
+		this.fDoorState = true;
+		System.out.println("Floor door opened");
+	}
+
+	/**
+	 * 
+	 */
+	public void floorDoorClose() {
+		this.fDoorState = false;
+		System.out.println("Floor door closed");
 	}
 	
 }
