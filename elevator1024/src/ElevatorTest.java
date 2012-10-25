eee6533c65f9685e330a984456967602f6f0f4ad
adrichdmello <adrichdmello@gmail.com>
2012-10-25 09:14:37 +0400

    ElevatorTest

---
+++ elevator1024/src/ElevatorTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ElevatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testElevator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsElevState() {
+		fail("Not yet implemented");
+	}
+
+}
